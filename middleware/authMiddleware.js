f89f027a7fdd2e50e63d38724a1a8f3d1328f94f
ADEDANCE <uthmanadedance@gmail.com>
2026-08-04 15:20:35 +0100

    Refractor : change spelling error

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,10 +1,14 @@
 // impoert JWT
 const jwt = require("jsonwebtoken");
+// import user model
+const User = require("../models/User");
 
-const protect = (req, res, next) => {
+const protect = async (req, res, next) => {
   try {
     // get the Authorization header
     const authHeader = req.headers.authorization;
+
+    console.log("Authorization Header:", authHeader);
 
     // Check if a token was provided
     if (!authHeader) {
@@ -13,9 +17,10 @@ const protect = (req, res, next) => {
       });
     }
 
-
     // Extract the actual token
     const token = authHeader.split(" ")[1];
+
+    console.log("Extracted Token:", token);
 
     if (!token) {
       return res.status(401).json({
@@ -23,12 +28,20 @@ const protect = (req, res, next) => {
       });
     }
 
-
     // Verify the JWT
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+    // Find the user
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res.status(401).json({
+        message: "User no longer exists",
+      });
+    }
+
     // Attach the user to the request
-    req.user = decoded;
+    req.user = user;
 
     next();
   } catch (error) {
