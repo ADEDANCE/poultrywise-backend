953a0f78a3ebb9a1d072b4fd895c40a9da6c852f
ADEDANCE <uthmanadedance@gmail.com>
2026-08-05 14:05:40 +0100

    Feat:Add updateflock function

--- controllers/flockController.js
+++ controllers/flockController.js
@@ -48,6 +48,77 @@ const createFlock = async (req, res) => {
   }
 };
 
+// get flock
+const getActiveFlock = async (req, res) => {
+  try {
+    const flock = await Flock.findOne({
+      user: req.user._id,
+      status: "active",
+    });
+
+    if (!flock) {
+      return res.status(404).json({
+        message: "No active flock found",
+      });
+    }
+
+    res.status(200).json({
+      flock,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to get active flock",
+      error: error.message,
+    });
+  }
+};
+
+// update flock stage
+
+const updateFlockStage = async (req, res) => {
+  try {
+    // get new stage
+    const { currentStage } = req.body;
+
+    // find farmer's active flock
+    const flock = await Flock.findOne({
+      user: req.user._id,
+      status: "active",
+    });
+
+    // Check if the flock exists
+
+    if (!flock) {
+      return res.status(404).json({
+        message: "No active flock found",
+      });
+    }
+
+    // Validate the stage
+    if (!["day-old", "grower", "layer"].includes(currentStage)) {
+      return res.status(400).json({
+        message: "Invalid flock stage",
+      });
+    }
+
+    flock.currentStage = currentStage;
+
+    await flock.save();
+
+    res.status(200).json({
+      message: "Flock stage updated successfully",
+      flock,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to update flock stage",
+      error: error.message,
+    });
+  }
+};
+
 module.exports = {
   createFlock,
+  getActiveFlock,
+  updateFlockStage,
 };
