a86c95815cebafab85fefc63b660dfa109f7bc62
ADEDANCE <uthmanadedance@gmail.com>
2026-08-07 23:01:32 +0100

    calculate and export mortality rate

--- services/dashboardService.js
+++ services/dashboardService.js
@@ -1,11 +1,26 @@
 // import DailyRecord
 const DailyRecord = require("../models/DailyRecord");
 
+const Flock = require("../models/Flock");
+
 const getDashboardSummary = async (userId) => {
   // Read all daily records for the logged-in user
   const dailyRecords = await DailyRecord.find({
     user: userId,
   });
+
+  // get active flock
+  const activeFlock = await Flock.findOne({
+    user: userId,
+    status: "active",
+  });
+
+  // Check if an active flock exists
+  if (!activeFlock) {
+    return {
+      message: "No active flock found",
+    };
+  }
 
   // Check if the farmer has any records
   if (dailyRecords.length === 0) {
@@ -21,6 +36,13 @@ const getDashboardSummary = async (userId) => {
   // Variables to store totals
   let totalExpenses = 0;
   let totalRevenue = 0;
+
+  let recommendedStage;
+  let totalMortality = 0;
+  const BROODING_MAX_DAYS = 56;
+  const GROWER_MAX_DAYS = 126;
+
+  let stageMessage = "";
 
   // Loop through every daily record
   for (const record of dailyRecords) {
@@ -43,17 +65,54 @@ const getDashboardSummary = async (userId) => {
 
     // Add today's revenue to the running total
     totalRevenue += dailyRevenue;
+    totalMortality += record.mortality;
   }
 
   // Calculate financial summary
   const netProfit = totalRevenue - totalExpenses;
 
-const roi =
-  totalExpenses === 0
-    ? 0
-    : Number(((netProfit / totalExpenses) * 100).toFixed(2));
+  const roi =
+    totalExpenses === 0
+      ? 0
+      : Number(((netProfit / totalExpenses) * 100).toFixed(2));
   // Determine flock status
   const status = netProfit >= 0 ? "Profitable" : "Not Profitable";
+
+  // Get today's date
+  const today = new Date();
+
+  // Get the flock's received date
+  const receivedDate = activeFlock.dateReceived;
+  // Calculate the difference
+  const differenceInMilliseconds = today - receivedDate;
+
+  // Convert to days
+  const birdAgeDays = Math.floor(
+    differenceInMilliseconds / (1000 * 60 * 60 * 24),
+  );
+
+  // Convert days to weeks
+  const birdAgeWeeks = Math.floor(birdAgeDays / 7);
+
+  if (birdAgeDays <= BROODING_MAX_DAYS) {
+    recommendedStage = "brooding";
+  } else if (birdAgeDays <= GROWER_MAX_DAYS) {
+    recommendedStage = "grower";
+  } else {
+    recommendedStage = "layer";
+  }
+
+  const stageChangeRequired = activeFlock.currentStage !== recommendedStage;
+
+  if (stageChangeRequired) {
+    stageMessage = `Your flock is ready to move to the ${recommendedStage} stage.`;
+  }
+
+  const birdsAlive = activeFlock.numberOfBirds - totalMortality;
+
+  const mortalityRate = Number(
+    ((totalMortality / activeFlock.numberOfBirds) * 100).toFixed(2),
+  );
 
   return {
     totalExpenses,
@@ -61,6 +120,21 @@ const roi =
     netProfit,
     roi,
     status,
+
+    birdsAlive,
+    totalMortality,
+    mortalityRate,
+
+    birdAgeDays,
+    birdAgeWeeks,
+
+    currentStage: activeFlock.currentStage,
+
+    recommendedStage,
+
+    stageChangeRequired,
+
+    stageMessage,
   };
 };
 
