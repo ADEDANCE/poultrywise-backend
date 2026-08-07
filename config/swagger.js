e9ba110852e695fcaa3612b4f18377d89105fd73
ADEDANCE <uthmanadedance@gmail.com>
2026-08-08 00:43:18 +0100

    Install swagger

---
+++ config/swagger.js
@@ -0,0 +1,29 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+
+    // API information
+    info: {
+      title: "PoultryWise API",
+      version: "1.0.0",
+      description:
+        "Backend API for PoultryWise, a poultry farm management application.",
+    },
+
+    servers: [
+      {
+        url: "http://localhost:3000",
+        description: "Local development server",
+      },
+    ],
+  },
+
+  apis: ["./routes/*.js"],
+};
+
+// generate the specification
+const swaggerSpec = swaggerJsdoc(options);
+
+module.exports = swaggerSpec;
