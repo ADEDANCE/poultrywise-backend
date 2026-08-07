dd268fe39a890dbba8bb65d9ac2caa07e56e279c
ADEDANCE <uthmanadedance@gmail.com>
2026-08-08 00:30:54 +0100

    Feat: Create getFlockHistory route

--- routes/flockRoutes.js
+++ routes/flockRoutes.js
@@ -9,11 +9,13 @@ const {
   getActiveFlock,
   updateFlockStage,
   completeFlock,
+  getFlockHistory,
 } = require("../controllers/flockController");
 
 router.post("/", protect, createFlock);
 router.get("/", protect, getActiveFlock);
 router.patch("/stage", protect, updateFlockStage);
 router.patch("/complete", protect, completeFlock);
+router.get("/history", protect, getFlockHistory);
 
 module.exports = router;
